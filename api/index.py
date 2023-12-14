5144e6dd7dd15d661c7cfa2486aeab2ef4fa62cb
hamza djamila amani <djamila.hamza@ensia.edu.dz>
2023-12-14 20:19:27 +0000

    intialization

--- api/index.py
+++ api/index.py
@@ -10,8 +10,8 @@ supabase: Client = create_client(url, key)
 
 @app.route('/users.signup',methods=['GET','POST'])
 def api_users_signup():
-    email= request.args.get('email')
-    password= request.args.get('password')
+    email= request.form.get('email')
+    password= request.form.get('password')
     error =False
     if (not email) or (len(email)<5): #You can even check with regx
         error='Email needs to be valid'
@@ -25,16 +25,18 @@ def api_users_signup():
         response = supabase.table('users').insert({"email": email, "password": password}).execute()
         print(str(response.data))
         if len(response.data)==0:
+
             error='Error creating the user'        
     if error:
         return json.dumps({'status':500,'message':error})        
     
-    return json.dumps({'status':200,'message':'','data':response.data})
+    return json.dumps({'status':200,'message':'','data':response.data[0]})
 
 @app.route('/users.login',methods=['GET','POST'])
 def api_users_login():
-    email= request.args.get('email')
-    password= request.args.get('password')
+    
+    email= request.form.get('email')
+    password= request.form.get('password')
     error =False
     if (not email) or (len(email)<5): #You can even check with regx
         error='Email needs to be valid'
@@ -43,7 +45,7 @@ def api_users_login():
     if (not error):    
         response = supabase.table('users').select("*").ilike('email', email).eq('password',password).execute()
         if len(response.data)>0:
-            return json.dumps({'status':200,'message':'','data':response.data})
+            return json.dumps({'status':200,'message':'','data':response.data[0]})
                
     if not error:
          error='Invalid Email or password'        
@@ -51,6 +53,22 @@ def api_users_login():
     return json.dumps({'status':500,'message':error})        
     
 
+@app.route('/users.signup.auth',methods=['GET','POST'])
+def api_users_signup_auth():
+    email= request.args.get('email')
+    password= request.args.get('password')
+    response = supabase.auth.sign_up({"email": email, "password": password})        
+    print(str(response))    
+    return str(response)
+
 @app.route('/')
 def about():
     return 'Welcome ENSIA Students'
+
+
+
+
+
+if __name__ == "__main__":
+   app.run(port=5000)
+
